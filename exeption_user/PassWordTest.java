4ccc2cc303b40d2ca842b0a5eb70c205d1c2f6ea
HeLLo2 <popt0@naver.com>
2021-12-14 23:27:28 +0900

    today's study code3
    user exception

---
+++ exeption_user/PassWordTest.java
@@ -0,0 +1,9 @@
+package exeption_user;
+
+public class PassWordTest {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ exeption_user/PasswordException.java
@@ -0,0 +1,8 @@
+package exeption_user;
+
+public class PasswordException extends IllegalArgumentException{
+	
+	public PasswordException(String message) {
+		super(message);
+	}
+}
