ecbb899f4aebb3822e5c956a5e9fb4444e7b8ebb
HeLLo2 <popt0@naver.com>
2021-12-15 23:04:55 +0900

    today's study 2021/12/15
    how to do logging in java

---
+++ logger/LoggerTest.java
@@ -0,0 +1,12 @@
+package logger;
+
+public class LoggerTest {
+
+	public static void main(String[] args) {
+			MyLogger myLogger = MyLogger.getLogger();
+			
+//			myLogger.log("test");
+			myLogger.fine("what!");
+	}
+
+}
