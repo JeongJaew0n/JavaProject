1d4f1e4757d94c9bb301c32e32d3a08f7d4cc17b
HeLLo2 <popt0@naver.com>
2021-12-22 23:46:50 +0900

    upgrade

--- easy_project/gradeSystem/GenerateGradeReport.java
+++ easy_project/gradeSystem/GenerateGradeReport.java
@@ -46,31 +46,39 @@ public class GenerateGradeReport {
 		}
 	}
 
-	public void getScoreGrade(Student student, int subjectId) {
-		ArrayList<Score> scoreList = student.getScoreList();
-		int majorId = student.getMajorSubejct().getSubjectid();
-
-		GradeEvaluation[] gradeEvaluation = { new BasicEvaluation(), new MajorEvaluation() }; // 학점 평가 클래스들
+	public void getScoreGrade(Student student, int subjectId) {	//점수 출력을 위한 함수
+		
+		ArrayList<Score> scoreList = student.getScoreList();	//입력받은 학생 객체의 스코어들을 가져옴
+		
+		int majorId = student.getMajorSubejct().getSubjectid();	//중점과목 입력받음
+		
+		GradeEvaluation[] gradeEvaluation = { new BasicEvaluation(), new MajorEvaluation(), new PassFailEvaluation() }; // 학점 평가 클래스들
 
 		for (int i = 0; i < scoreList.size(); i++) {
 
 			Score score = scoreList.get(i);
 
-			if (score.getSubject().getSubjectid() == subjectId) {
+			if (score.getSubject().getSubjectid() == subjectId) {	//함수의 인자로 넘겨받은 과목과 현재 선택된 점수의 과목과 일치하는지(수학점수 출력을 위해 호출 되었고 학생의 점수 목록중 수학 점수만을 가려내기 위한 조건문).
 
 				String grade = null;
 
-				if (score.getSubject().getSubjectid() == majorId) {
+				if (score.getSubject().getSubjectid() == majorId) {	//중점 과목이라면
 					grade = gradeEvaluation[Define.SAB_TYPE].getGrade(score.getPoint());
-				} else {
-					grade = gradeEvaluation[Define.AB_TYPE].getGrade(score.getPoint());
-
-					buffer.append(score.getPoint());
-					buffer.append(":");
-					buffer.append(grade);
-					buffer.append(" | ");
-
 				}
+				
+				else if(score.getSubject().getSubjectid() == Define.ENGLISH) {
+					grade = gradeEvaluation[Define.PF_TYPE].getGrade(score.getPoint());
+				}
+				
+				else{
+					grade = gradeEvaluation[Define.AB_TYPE].getGrade(score.getPoint());
+				}
+				
+				buffer.append(score.getPoint());
+				buffer.append(":");
+				buffer.append(grade);
+				buffer.append(" | ");
+
 			}
 
 		}
