e196f55e364084c21ac18cebe7d4d75a2d5e51e4
HeLLo2 <popt0@naver.com>
2021-12-25 23:46:32 +0900

    Drawing a diamond with an user input
    if you input a number that type is an integer this program is going to draw a diamond

---
+++ Algorithm/draw_star/stars_leve1.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+class dia {
+	void draw(int n) {
+		System.out.println("drawing...");
+		char[] data = new char[n];
+		for(int i=0; i<n; i++) {
+			data[i] = ' ';
+		}
+		for(int i=0; i<n/2+1; i++) {
+			data[n/2 + i] = '*';
+			data[n/2 - i] = '*';
+			System.out.println(data);
+		}
+		for(int i=n/2; i>0; i--) {
+			data[n/2 + i] = ' ';
+			data[n/2 - i] = ' ';
+			System.out.println(data);
+		}
+	}
+}
+
+public class stars_leve1{
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		dia d = new dia();
+		
+		System.out.print("Input number: ");
+		
+		String s = br.readLine();
+		br.close();
+		int num = Integer.parseInt(s);
+
+		System.out.println(num);
+		
+		d.draw(num);
+
+	}
+
+}
