f7b4d32819dea71fe3017b43e427f03ec82e3a01
HeLLo2 <popt0@naver.com>
2021-12-27 22:52:41 +0900

    review and add comments
    For remember

--- thread/SyncMain.java
+++ thread/SyncMain.java
@@ -1,7 +1,7 @@
 package thread;
 
-class Bank {
-	private int money = 10000;
+class Bank {	//동기화(synchronization)를 공부하기 위한 클래스.
+	private int money = 10000;	//Pong과 PongWife는 두 클래스 모두 이 변수를 참조하는 메서드를 사용한다.
 	public static int king = 100;
 	
 	public void saveMoney(int save) {
@@ -46,16 +46,16 @@ class Bank {
 }
 
 class Pong extends Thread {
-	public void run() {
-		synchronized (SyncMain.myBank) {
-			System.out.println("start save");
-			SyncMain.myBank.saveMoney(3000);
+	public void run() {	
+		synchronized (SyncMain.myBank) {	//Pong클래스에서 스레드(Thread)를 실행한다. 해당 스레드는 전역변수인 myBank 객체를 다른 스레드에서 사용중이지 않을때 동작한다.
+			System.out.println("start save");	
+			SyncMain.myBank.saveMoney(3000);	//static으로 공유되는 전역변수 myBank의 객체의 saveMoney 메서드를 실행한다.
 			System.out.println("saveMoney(3000): " + SyncMain.myBank.getMoney());
 		}
 
 	}
 }
-class PongWife extends Thread {
+class PongWife extends Thread {	//Pong클래스와 동일.
 	public void run() {
 		synchronized (SyncMain.myBank) {
 			System.out.println("start minus");
