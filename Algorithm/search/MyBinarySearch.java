837c69b92f4f9030f72a4de0c27940549dc329b2
HeLLo2 <popt0@naver.com>
2022-01-16 18:49:33 +0900

    Algorithm-- Binary Search is so easy!

---
+++ Algorithm/search/MyBinarySearch.java
@@ -0,0 +1,44 @@
+package search;
+
+class test {
+    public static void main(String[] args) {
+        int[] testArray = new int[10];
+        for (int i = 0; i < testArray.length; i++) {
+            testArray[i] = i*2;
+        }
+        for (int i : testArray) {
+            System.out.print(i + " ");
+        }
+
+        int findingNumber = 5;
+        int k = MyBinarySearch.binSearch(testArray, findingNumber);
+
+        System.out.println("\n 찾는 숫자: " + findingNumber + "/찾은 숫자: " + k);
+    }
+}
+
+public class MyBinarySearch {
+    static int binSearch(int[] intArray, int findingNumber) {
+        int m = intArray.length/2;
+        int L = 0;
+        int R = intArray.length - 1;
+
+        int foundNumber;
+
+        while (true) {
+            if(L>=R || intArray[m] == findingNumber) {
+                foundNumber = intArray[m];
+                break;
+            }
+            if(intArray[m] < findingNumber) {
+                L = m+1;
+                m = (L+R)/2;
+                continue;
+            }
+            R = m-1;
+            m = (L+R)/2;
+        }
+        return foundNumber;
+    }
+}
+
