32283aad531d50d1c8f49699a505e0e3f6b77142
HeLLo2 <popt0@naver.com>
2022-01-21 23:22:06 +0900

    DataStructure Study Queue

--- DataStructure/queue/MyQueue.java
+++ DataStructure/queue/MyQueue.java
@@ -1,22 +1,50 @@
 package queue;
 
+import java.util.Arrays;
 import java.util.Queue;
 
 public class MyQueue<E> implements QueueInterface<E> {
-    Object[] datas;
+    
+    Object[] datas; // 데이터가 저장될 배열
+    private int capacity = 10;
+
+    private final int front = 0;  // Queue 앞쪽
+    private int rear = 1;   // Queue 뒤쪽, 데이터의 갯수
+
+    MyQueue() {
+        datas = new Object[capacity];
+    }
+
+    MyQueue(int capacity) {
+        datas = new Object[capacity];
+    }
 
     @Override
-    public boolean offer(E e) {
-        return false;
+    public boolean offer(E e) { // Enqueue
+        datas[rear-1] = e;
+        rear++;
+        return true;
     }
 
     @Override
     public E poll() {
-        return null;
+        if(rear == 0 )
+            return null;
+
+        Object removedData = datas[front];
+
+        for(int i=0; i<rear-1; i++) {
+            datas[i] = datas[i+1];
+        }
+        datas[rear-1] = null;
+        rear--;
+        return (E)removedData;
     }
 
     @Override
     public E peek() {
-        return null;
+        if(rear == 0 )
+            return null;
+        return (E) datas[front];
     }
 }
---
+++ DataStructure/queue/QueueMain.java
@@ -0,0 +1,16 @@
+package queue;
+
+public class QueueMain {
+    public static void main(String[] args) {
+        MyQueue<String> myQueue = new MyQueue<>(5);
+
+        System.out.println(myQueue.offer("First Data"));
+        System.out.println(myQueue.offer("Second Data"));
+        System.out.println(myQueue.offer("Third Data"));
+
+        System.out.println(myQueue.peek());
+        System.out.println(myQueue.poll());
+        System.out.println(myQueue.peek());
+
+    }
+}
