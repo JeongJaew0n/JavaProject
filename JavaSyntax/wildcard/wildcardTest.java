c4bbf90e2675fb14540630a4adaf065b3593c05c
HeLLo2 <popt0@naver.com>
2022-01-22 23:50:31 +0900

    study Java wild card

---
+++ JavaSyntax/wildcard/wildcardTest.java
@@ -0,0 +1,19 @@
+package wildcard;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class wildcardTest<T> {
+    public static void main(String[] args) {
+        Collection<Integer> testColllection = new ArrayList<>();
+        testColllection.add(1233);
+        printCollection(testColllection);
+    }
+
+    static void printCollection(Collection<? extends Number> c) {
+        for (Number e : c) {
+            System.out.print(e);
+        }
+    }
+
+}
