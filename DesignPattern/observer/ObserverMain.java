236bb44d1a6f1f84720e0cee72bff9372bc03acf
HeLLo2 <popt0@naver.com>
2022-01-26 23:54:42 +0900

    DesignPattern study Observer Pattern

--- DesignPattern/observer/ObserverMain.java
+++ DesignPattern/observer/ObserverMain.java
@@ -5,11 +5,16 @@ public class ObserverMain {
     public static void main(String[] args){
         Button button = new Button("버튼");
 
-        button.addListener({
+        button.addListener(new IButtonListener() {
             @Override
             public void clickEvent(String event) {
-                System.out.println(event);
             }
         });
+
+        button.click("메세지 전달: click1");
+        button.click("메세지 전달: click2");
+        button.click("메세지 전달: click3");
+        button.click("메세지 전달: click4");
+
     }
 }
