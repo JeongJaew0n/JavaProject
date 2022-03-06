7ad8aa8eac52dc43095851a264fcb76bcd7a049f
HeLLo2 <popt0@naver.com>
2022-03-06 22:37:10 +0900

    Update Solution.java

--- CodingTest/kakao/recuritment2022/Q1/Solution.java
+++ CodingTest/kakao/recuritment2022/Q1/Solution.java
@@ -1,7 +1,7 @@
 package kakao.recuritment2022.Q1;
 
 import java.util.StringTokenizer;
-
+//
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = {};
